babe5d6195cd4a2cb7762fb784df094a07aa13b4
Jhonata Fabricio <97449786+JhFabricio@users.noreply.github.com>
2022-06-01 23:39:03 -0300

    Commit JavaScript
    Implementei adicionando as funções no arquivo JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const contentResult = document.querySelector(".content-result")
+const btnConfirm = document.querySelector(".btn-confirm");
 
+
+function contentClean() {
+    //alert("ola mundo")
+    contentResult.innerHTML  = "";
+    
+}
+
+function backSpace() {
+    if (contentResult.textContent) {
+        let result = document.getElementById("result").innerHTML
+        contentResult.innerHTML = result.substring(0, result.length - 1);
+        
+    }
+    
+}
+
+function insert(valor) {
+    contentResult.innerHTML += valor;
+    
+}
+
+function confirm() {
+    if (contentResult.textContent != 'Error') {
+        document.getElementById("result").innerHTML = eval(contentResult.innerHTML);
+        
+    }
+    
+}
